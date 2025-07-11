df362bccf3c0207a6e255a43e75e2bb1dbbf1beb
McDonald <odiasemcdonald1@gmail.com>
2025-07-11 16:15:55 +0100

    Create specific components in React App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage.jsx'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +13,9 @@ function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
