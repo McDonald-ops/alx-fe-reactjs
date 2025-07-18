62832709c23d82df6b1fabb89ddf859d7c44f156
McDonald <odiasemcdonald1@gmail.com>
2025-07-18 22:18:17 +0100

    updated inline styling

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <p style={{fontSize:'1rem', color:'orange' }}>© <span style={{fontSize:'1rem', color:'white', fontStyle:'italic'}}>2023 City Lovers</span></p>
+      <p style={{fontSize:'1rem', color:'white', fontStyle:'italic' }}>© 2023 City Lovers</p>
     </footer>
   );
 }
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
     <main style={{backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px'}}>
-      <p style={{fontStyle:'italic', fontSize:'1.5rem'}}>I love to visit <span style={{fontWeight:'bold', color:'orange', fontStyle:'revert-layer'}}>New York</span>, Paris, and Tokyo.</p>
+      <p style={{fontStyle:'italic', fontSize:'1.5rem'}}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
