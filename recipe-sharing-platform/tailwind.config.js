2750e228f513a69393d6bfd8a8c09f0e0a28686c
McDonald <odiasemcdonald1@gmail.com>
2025-08-11 12:00:44 +0100

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+ module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html', './index.html'],
+  darkMode: 'class', // You can set it to 'media' or 'false' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
